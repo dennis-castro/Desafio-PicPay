502dd1a885cd867477f0569e59cbcd3753d9d141
Dennis Castro <d3nnis_castro@hotmail.com>
2023-09-12 21:38:06 -0300

    TransactionRepository

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionsRepository.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.picpaysimplificado.repositories;
+
+
+import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionsRepository extends JpaRepository<Transaction, Long> {
+
+
+}
